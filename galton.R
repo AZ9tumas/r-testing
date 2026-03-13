770f3e79fb012a6455377e5f551684bbd7344528
AZ9tumas <rounakdas2025@gmail.com>
2026-03-14 01:32:18 +0530

    added more visuals

--- galton.R
+++ galton.R
@@ -18,10 +18,19 @@ peg.df <- do.call(rbind, peg_coordinates)
 text.df <- data.frame(y = rep(0, n), x = seq(1, 2 * n - 1, by = 2), label = 1:n)
 
 pegviz1 <- ggplot() +
-    geom_point(data = peg.df, aes(x = x, y = y), size = 5, shape = 17, color = "green") + 
-    geom_text(data = text.df, aes(x = x, y = y, label = label))
-    # + theme_void(base_size = 14)
+  geom_point(data = peg.df, aes(x = x, y = y), size = 5, shape = 21, color = "green") + 
+  geom_text(data = text.df, aes(x = x, y = y, label = label)) +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    
+    panel.background = element_rect(fill = "transparent", colour = NA),
+    plot.background = element_rect(fill = "transparent", colour = NA)
+  )
 
 animint2dir(list(
     pegs = pegviz1
 ), out.dir = "galton-animint")
+
